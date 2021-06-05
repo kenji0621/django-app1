e29e56090242a7d1137972d3c658dbe3d518a921
Your Name <you@example.com>
2021-06-05 01:12:24 +0000

    -sann

--- website/views.py
+++ website/views.py
@@ -6,7 +6,7 @@ class IndexView(TemplateView):
     
     def get_context_data(self):
         ctxt = super().get_context_data()
-        ctxt["username"] = "みなさん"
+        ctxt["username"] = "みな"
         return ctxt
     
 
